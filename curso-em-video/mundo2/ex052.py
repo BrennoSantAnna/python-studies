6ffe0d08aa817c744a5d1f9f26302c1a895d6097
BrennoSantAnna <brennopsantanna1@gmail.com>
2026-02-03 09:44:03 -0300

    Colors added for better visualization

--- curso-em-video/mundo2/ex052.py
+++ curso-em-video/mundo2/ex052.py
@@ -7,13 +7,13 @@ total = 0
 
 for contador in range(1, numero + 1):
     if numero % contador == 0:
-        print(end="")
+        print("\033[33m", end="")
         total += 1
     else:
-        print(end="")
+        print("\033[31m", end="")
     print(f"{contador} ", end="")
 
-print(f"\nO número {numero} foi divisível {total} vezes")
+print(f"\n\033[mO número {numero} foi divisível {total} vezes")
 
 if total == 2:
     print(f"E por isso ele é PRIMO!")
