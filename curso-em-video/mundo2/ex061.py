bda771e6200ae24e98d2f4bdec583882684d2c90
BrennoSantAnna <brennopsantanna1@gmail.com>
2026-02-04 12:29:05 -0300

    World 2 Exercises

---
+++ curso-em-video/mundo2/ex061.py
@@ -0,0 +1,18 @@
+# Refaça o desafio 051, lendo o primeiro termo e a razão de uma PA,
+# mostrando os 10 primeiros termos da prograssão usando a estrutura while
+
+print("Gerador de PA")
+print("-=" * 10)
+
+primeiro_termo = int(input("Primeiro termo: "))
+razao = int(input("Razão da PA: "))
+
+termo = primeiro_termo
+contador = 1
+
+while contador <= 10:
+    print(f"{termo} -> ", end="")
+    termo += razao
+    contador += 1
+    
+print("ACABOU")
