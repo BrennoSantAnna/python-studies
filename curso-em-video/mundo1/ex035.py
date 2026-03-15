a9fb295d980c91a658f62eb88c7a70b716e052e7
Brenno SantAnna <brennopsantanna1@gmail.com>
2026-03-15 17:08:11 -0300

    refactor: modularize triangle analyzer and add input validation

--- curso-em-video/mundo1/ex035.py
+++ curso-em-video/mundo1/ex035.py
@@ -1,16 +1,45 @@
 # Desenvolva um programa que leia o comprimento
-# de três retas e diga ao usuário se elas podem 
+# de três retas e diga ao usuário se elas podem
 # ou não formar um triângulo
 
-print("-=" * 13)
-print("Analisador de Triângulos")
-print("-=" * 13)
 
-a = float(input("Primeiro segmento: "))
-b = float(input("Segundo segmento: "))
-c = float(input("Terceiro segmento: "))
+def menu() -> None:
+    print("-=" * 13)
+    print("Triangle Analyzer")
+    print("-=" * 13)
 
-if a < b + c and b < a + c and c < a + b:
-    print("Os segmentos acima PODEM FORMAR triângulo!")
-else:
-    print("Os segmentos NÃO PODEM FORMAR triângulo")
+
+def get_segments(prompt1: str, prompt2: str, prompt3: str) -> tuple[int, int, int]:
+    while True:
+        try:
+            a = float(input(prompt1))
+            b = float(input(prompt2))
+            c = float(input(prompt3))
+
+            return a, b, c
+
+        except ValueError:
+            print("Please enter valid integers.")
+
+
+def is_triangle(a: int, b: int, c: int) -> None:
+    return a < b + c and b < a + c and c < a + b
+
+
+def main() -> None:
+    menu()
+
+    a, b, c = get_segments(
+        "First value: ",
+        "Second value: ",
+        "Third value: "
+    )
+
+    if is_triangle(a, b, c):
+        print("The segments above may form a triangle!")
+    else:
+        print("The segments above cannot form a triangle!")
+
+
+if __name__ == "__main__":
+    main()
